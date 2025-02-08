d9651b5d71ac10a8cbb6d7263ad5155f780f4fc8
TSKailash <tskailash20@gmail.com>
2025-02-08 09:26:49 +0530

    fix

--- frontend/hackathon/src/components/Home.jsx
+++ frontend/hackathon/src/components/Home.jsx
@@ -17,7 +17,7 @@ export default function Home({ user }) {
           href="http://localhost:5000/api/logout"
           className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors"
         >
-          Logout
+          Logout Now
         </a>
       </div>
     </div>
